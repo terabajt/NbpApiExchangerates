abda15d39f010133031381f608b8defb209b8568
Michał Pasynek <terabajt@terabajtlw.pl>
2024-12-05 13:29:55 +0100

    fix: Added CORS configuration to allow requests from frontend (port 4200)

--- src/main/java/com/pasynekmichal/controller/CurrencyController.java
+++ src/main/java/com/pasynekmichal/controller/CurrencyController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("currencies")
 public class CurrencyController {
     private final CurrencyService service;
