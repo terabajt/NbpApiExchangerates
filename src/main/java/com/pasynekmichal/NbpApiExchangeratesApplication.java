3abbaa23beda2298d564030be96b778cd45cdfea
Michał Pasynek <terabajt@terabajtlw.pl>
2024-12-10 13:17:07 +0100

    fix: Recovery main file

--- src/main/java/com/pasynekmichal/NbpApiExchangeratesApplication.java
+++ src/main/java/com/pasynekmichal/NbpApiExchangeratesApplication.java
@@ -1,14 +1,9 @@
 package com.pasynekmichal;
-
-import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class NbpApiExchangeratesApplicationTests {
-
-	@Test
-	void contextLoads() {
+@SpringBootApplication
+public class NbpApiExchangeratesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NbpApiExchangeratesApplication.class, args);
 	}
 }
